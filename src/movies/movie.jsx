e4545bbe164c6301906ea5848a8d0a6532189660
Rashid075 <108038076+Rashid075@users.noreply.github.com>
2024-09-04 23:08:16 +0530

    added env variavles

--- src/movies/movie.jsx
+++ src/movies/movie.jsx
@@ -1,29 +1,34 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import MovieCard from './movie_card';
-import MovieDialog from './movie_dialog';
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import MovieCard from "./movie_card";
+import MovieDialog from "./movie_dialog";
 
 const Movie = () => {
-  const [search, setSearch] = useState('');
+  const [search, setSearch] = useState("");
   const [movies, setMovies] = useState([]);
   const [suggestions, setSuggestions] = useState([]);
-  const [sortBy, setSortBy] = useState('popularity.desc');
+  const [sortBy, setSortBy] = useState("popularity.desc");
   const [genre, setGenre] = useState([]);
-  const [selectedGenre, setSelectedGenre] = useState('');
+  const [selectedGenre, setSelectedGenre] = useState("");
   const [selectedMovie, setSelectedMovie] = useState(null);
   const [favorites, setFavorites] = useState([]);
   const [showFavorites, setShowFavorites] = useState(false);
-  const [noResults, setNoResults] = useState(false); 
-  
+  const [noResults, setNoResults] = useState(false);
+
   useEffect(() => {
+    // console.log(import.meta.env.VITE_API_KEY);
+
     const fetchGenres = async () => {
       try {
-        const response = await axios.get('https://api.themoviedb.org/3/genre/movie/list', {
-          params: { api_key: '8b10a322ddf1cf635c8604c48ef70304' },
-        });
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/genre/movie/list",
+          {
+            params: { api_key: import.meta.env.VITE_API_KEY },
+          }
+        );
         setGenre(response.data.genres);
       } catch (error) {
-        console.error('Error fetching genres:', error);
+        console.error("Error fetching genres:", error);
       }
     };
     fetchGenres();
@@ -32,19 +37,24 @@ const Movie = () => {
   useEffect(() => {
     const fetchMovies = async () => {
       try {
-        const response = await axios.get('https://api.themoviedb.org/3/discover/movie', {
-          params: {
-            api_key: '8b10a322ddf1cf635c8604c48ef70304',
-            sort_by: sortBy,
-            with_genres: selectedGenre,
-            query: search,
-            page: 1,
-          },
-        });
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/discover/movie",
+          {
+            params: {
+              api_key: import.meta.env.VITE_API_KEY,
+              sort_by: sortBy,
+              with_genres: selectedGenre,
+              query: search,
+              page: 1,
+            },
+          }
+        );
         setMovies(response.data.results);
-        setNoResults(response.data.results.length === 0 && search.trim() !== '');
+        setNoResults(
+          response.data.results.length === 0 && search.trim() !== ""
+        );
       } catch (error) {
-        console.error('Error fetching movies:', error);
+        console.error("Error fetching movies:", error);
       }
     };
     fetchMovies();
@@ -52,7 +62,7 @@ const Movie = () => {
 
   useEffect(() => {
     // Load favorites from localStorage
-    const savedFavorites = JSON.parse(localStorage.getItem('favorites')) || [];
+    const savedFavorites = JSON.parse(localStorage.getItem("favorites")) || [];
     setFavorites(savedFavorites);
   }, []);
 
@@ -62,13 +72,16 @@ const Movie = () => {
 
     if (query) {
       try {
-        const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
-          params: { api_key: '8b10a322ddf1cf635c8604c48ef70304', query },
-        });
+        const response = await axios.get(
+          "https://api.themoviedb.org/3/search/movie",
+          {
+            params: { api_key: import.meta.env.VITE_API_KEY, query },
+          }
+        );
         setSuggestions(response.data.results);
-        setNoResults(response.data.results.length === 0 && query.trim() !== '');
+        setNoResults(response.data.results.length === 0 && query.trim() !== "");
       } catch (error) {
-        console.error('Error fetching search suggestions:', error);
+        console.error("Error fetching search suggestions:", error);
       }
     } else {
       setSuggestions([]);
@@ -83,30 +96,39 @@ const Movie = () => {
     e.preventDefault();
 
     try {
-      const response = await axios.get('https://api.themoviedb.org/3/search/movie', {
-        params: { api_key: '8b10a322ddf1cf635c8604c48ef70304', query: search },
-      });
+      const response = await axios.get(
+        "https://api.themoviedb.org/3/search/movie",
+        {
+          params: {
+            api_key: import.meta.env.VITE_API_KEY,
+            query: search,
+          },
+        }
+      );
       setMovies(response.data.results);
       setSuggestions([]); // Clear suggestions after search submit
-      setNoResults(response.data.results.length === 0 && search.trim() !== '');
+      setNoResults(response.data.results.length === 0 && search.trim() !== "");
     } catch (error) {
-      console.error('Error searching movies:', error);
+      console.error("Error searching movies:", error);
     }
   };
 
   const handleMovieClick = async (movieId) => {
     try {
-      const response = await axios.get(`https://api.themoviedb.org/3/movie/${movieId}`, {
-        params: {
-          api_key: '8b10a322ddf1cf635c8604c48ef70304',
-          append_to_response: 'credits',
-        },
-      });
+      const response = await axios.get(
+        `https://api.themoviedb.org/3/movie/${movieId}`,
+        {
+          params: {
+            api_key: import.meta.env.VITE_API_KEY,
+            append_to_response: "credits",
+          },
+        }
+      );
       setSelectedMovie(response.data);
       setSuggestions([]); // Clear suggestions when a movie is selected
       setNoResults(false);
     } catch (error) {
-      console.error('Error fetching movie details:', error);
+      console.error("Error fetching movie details:", error);
     }
   };
 
@@ -118,38 +140,40 @@ const Movie = () => {
       : [...favorites, movie];
 
     setFavorites(updatedFavorites);
-    localStorage.setItem('favorites', JSON.stringify(updatedFavorites));
+    localStorage.setItem("favorites", JSON.stringify(updatedFavorites));
   };
 
   const handleShowFavorites = () => setShowFavorites(!showFavorites);
 
   return (
-    <div className='bg-gray-900 min-h-screen text-gray-100'>
-      <h1 className='text-center text-6xl p-10 text-blue-400 font-bold drop-shadow-lg'>MovieHouse</h1>
-      <div className='flex flex-col items-center'>
+    <div className="bg-gray-900 min-h-screen text-gray-100">
+      <h1 className="text-center text-6xl p-10 text-blue-400 font-bold drop-shadow-lg">
+        MovieHouse
+      </h1>
+      <div className="flex flex-col items-center">
         {/* Search Bar */}
-        <div className='w-full max-w-md mb-5'>
-          <div className='relative'>
+        <div className="w-full max-w-md mb-5">
+          <div className="relative">
             <input
-              type='text'
-              placeholder='Search movies...'
-              className='w-full border border-gray-700 bg-gray-800 text-gray-100 rounded-l-xl py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300'
+              type="text"
+              placeholder="Search movies..."
+              className="w-full border border-gray-700 bg-gray-800 text-gray-100 rounded-l-xl py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300"
               value={search}
               onChange={handleSearch}
             />
             <button
-              className='absolute right-0 top-0 h-full px-4 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 transition duration-300'
+              className="absolute right-0 top-0 h-full px-4 bg-blue-500 text-white rounded-r-lg hover:bg-blue-600 transition duration-300"
               onClick={handleSearchSubmit}
             >
               Search
             </button>
             {/* Auto-suggestion dropdown */}
             {suggestions.length > 0 && (
-              <div className='absolute w-full bg-gray-800 border border-gray-700 mt-1 rounded-lg z-10'>
+              <div className="absolute w-full bg-gray-800 border border-gray-700 mt-1 rounded-lg z-10">
                 {suggestions.map((suggestion) => (
                   <div
                     key={suggestion.id}
-                    className='p-2 cursor-pointer hover:bg-gray-700'
+                    className="p-2 cursor-pointer hover:bg-gray-700"
                     onClick={() => handleMovieClick(suggestion.id)}
                   >
                     {suggestion.title}
@@ -159,7 +183,7 @@ const Movie = () => {
             )}
             {/* No Results Message */}
             {noResults && (
-              <div className='absolute w-full bg-gray-800 border border-gray-700 p-2 rounded-lg z-10 text-center text-gray-400'>
+              <div className="absolute w-full bg-gray-800 border border-gray-700 p-2 rounded-lg z-10 text-center text-gray-400">
                 <p>No results found</p>
               </div>
             )}
@@ -167,31 +191,37 @@ const Movie = () => {
         </div>
 
         {/* Sort By and Genre Dropdowns */}
-        <div className='flex flex-wrap justify-center space-x-4 mb-5'>
-          <div className='mb-2'>
-            <label htmlFor='sort' className='sr-only'>Sort By:</label>
+        <div className="flex flex-wrap justify-center space-x-4 mb-5">
+          <div className="mb-2">
+            <label htmlFor="sort" className="sr-only">
+              Sort By:
+            </label>
             <select
-              id='sort'
-              className='border border-gray-700 bg-gray-800 text-gray-100 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300'
+              id="sort"
+              className="border border-gray-700 bg-gray-800 text-gray-100 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300"
               value={sortBy}
               onChange={handleSort}
             >
-              <option value='popularity.desc'>Sort by Popularity</option>
-              <option value='vote_average.desc'>Sort by Rating</option>
-              <option value='release_date.desc'>Sort by Release Date</option>
+              <option value="popularity.desc">Sort by Popularity</option>
+              <option value="vote_average.desc">Sort by Rating</option>
+              <option value="release_date.desc">Sort by Release Date</option>
             </select>
           </div>
-          <div className='mb-2'>
-            <label htmlFor='genre' className='sr-only'>Genre</label>
+          <div className="mb-2">
+            <label htmlFor="genre" className="sr-only">
+              Genre
+            </label>
             <select
-              id='genre'
-              className='border border-gray-700 bg-gray-800 text-gray-100 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300'
+              id="genre"
+              className="border border-gray-700 bg-gray-800 text-gray-100 rounded-lg py-2 px-4 focus:outline-none focus:border-blue-500 transition duration-300"
               value={selectedGenre}
               onChange={handleGenre}
             >
-              <option value=''>Select Genre</option>
+              <option value="">Select Genre</option>
               {genre.map((g) => (
-                <option key={g.id} value={g.id}>{g.name}</option>
+                <option key={g.id} value={g.id}>
+                  {g.name}
+                </option>
               ))}
             </select>
           </div>
@@ -199,14 +229,14 @@ const Movie = () => {
 
         {/* Favorites Button */}
         <button
-          className='mb-5 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300'
+          className="mb-5 bg-blue-500 text-white py-2 px-4 rounded-lg hover:bg-blue-600 transition duration-300"
           onClick={handleShowFavorites}
         >
-          {showFavorites ? 'Show All Movies' : 'Show Favorites'}
+          {showFavorites ? "Show All Movies" : "Show Favorites"}
         </button>
 
         {/* Movie Cards */}
-        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 p-4'>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3 gap-6 p-4">
           {(showFavorites ? favorites : movies).length > 0 ? (
             (showFavorites ? favorites : movies).map((movie) => (
               <MovieCard
@@ -218,7 +248,9 @@ const Movie = () => {
               />
             ))
           ) : noResults ? (
-            <p className='text-center text-gray-400 col-span-full text-5xl p-5'>No movies to display</p>
+            <p className="text-center text-gray-400 col-span-full text-5xl p-5">
+              No movies to display
+            </p>
           ) : null}
         </div>
       </div>
